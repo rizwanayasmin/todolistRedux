9f35f746d8f015c7caee4f0d1e700e74866f4cb3
rizwana <Rizwana@doodleblue.com>
2021-09-23 18:14:01 +0530

    todolist redux

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux";
+import visibilityFilter from "./VisibilityFilter";
+import todos from "./todos";
+
+export default combineReducers({ todos,visibilityFilter });
